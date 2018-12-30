20bc9f7464d3b1f226b2d6d5c4366cd10dc7de12
William Luo <gluo7777@gmail.com>
2018-12-30 02:11:39 -0600

    before refactoring to nodejs

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+const apiRouter = require('./routes/api');
+app.use('/api', apiRouter);
+
+module.exports = app;
