5efa630e1e92c9cbda898147c78a84ed36bd0c75
William Luo <gluo7777@gmail.com>
2019-01-01 12:42:14 -0600

    added favicon and modified run script

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@
 # to generating less-verbose error messages, setting the variable to production caches view templates and CSS files generated from CSS extensions. Tests 
 # indicate that setting NODE_ENV to production can improve app performance by a factor of three!
 export NODE_ENV=production
-PORT=3030 node bin/wwww
+app=portfolio
+PORT=3030 node bin/www > $app.out 2> $app.err
