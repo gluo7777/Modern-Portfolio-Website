6dfbe7f88ee2934701651db05a9e8c93b7c65b45
William Luo <gluo7777@gmail.com>
2019-01-03 01:29:14 +0000

    change to run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 # We can remove stack traces in error pages by setting the NODE_ENV environment variable to production (it is set to 'development' by default). In addition 
 # to generating less-verbose error messages, setting the variable to production caches view templates and CSS files generated from CSS extensions. Tests 
 # indicate that setting NODE_ENV to production can improve app performance by a factor of three!
+# npm install
+echo "Configuring environment"
 export NODE_ENV=production
 app=portfolio
-PORT=3030 node bin/www > $app.out 2> $app.err
+port=3030
+echo "Starting app=$app in port=$port"
+$(PORT=$port node bin/www > $app.out 2> $app.err) &
